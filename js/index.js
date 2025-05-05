be0d7a3aafb580e4be56f274780fa7bb97fe4abd
PepeSlime <pepegemeo8@gmail.com>
2025-05-05 11:42:40 -0300

    teste js

--- js/index.js
+++ js/index.js
@@ -2,14 +2,14 @@ const task1 = document.querySelector("#task1");
 const task2 = document.querySelector("#task2");
 const task3 = document.querySelector("#task3");
 
-task1.addEventListener("click", () => {
+task1.addEventListener("click", "touchend", () => {
     window.location.href = "biten-apple.html"; 
 });
 
-task2.addEventListener("click", () => {
+task2.addEventListener("click", "touchend", () => {
     window.location.href = "eaten-apple.html";
 });
 
-task3.addEventListener("click", () => {
+task3.addEventListener("click", "touchend", () => {
     window.location.href = "trash.html";
 });
